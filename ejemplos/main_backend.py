7ba082fdace9822312c9419c4feceafb0d87b23e
Sugheymartinez <sugheymartinez066@gmail.com>
2026-04-10 01:29:45 -0600

    Add files via upload

---
+++ ejemplos/main_backend.py
@@ -0,0 +1,12 @@
+from backend import*
+
+usuario1=Usuario("Gael",23,"Croquetas de atun")
+usuario2=Usuario("Dana",19,"Lassagna")
+usuario3=Usuario("Sughey",20,"Espaghetti verde")
+
+print(usuario1.mostrar_datos())
+print(usuario2.mostrar_datos())
+print(usuario3.mostrar_datos())
+
+print(Usuario.mostrar_lista())
+
